c71a01332d6367dfc16680ac504cefcd40591f00
Vanessa Cruz <vaneycarolinne.vc@gmail.com>
2017-06-02 09:00:34 -0400

    Dibujando con canvas y funciones en javascript

--- dibujo.js
+++ dibujo.js
@@ -1,17 +1,17 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100,100);
-lienzo.lineTo(200,200);
-lienzo.stroke();
-lienzo.closePath();
+dibujarLinea("pink", 10, 300, 220, 10);
+dibujarLinea("yellow", 300, 10, 10, 220);
 
-lienzo.beginPath();
-lienzo.strokeStyle = "blue";
-lienzo.moveTo(50,100);
-lienzo.lineTo(20,200);
-lienzo.stroke();
-lienzo.closePath();
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.moveTo(xinicial,yinicial);
+	lienzo.lineTo(xfinal,yfinal);
+	lienzo.stroke();
+	lienzo.closePath();
+}
 
