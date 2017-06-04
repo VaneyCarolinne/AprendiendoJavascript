63f85b9c8b7da3c129d2b677148c817576e371dc
Vanessa Cruz <vaneycarolinne.vc@gmail.com>
2017-06-03 23:30:53 -0400

    Dibujando lineas con ciclos for y while

--- dibujo.js
+++ dibujo.js
@@ -1,8 +1,22 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-
-dibujarLinea("pink", 10, 300, 220, 10);
-dibujarLinea("yellow", 300, 10, 10, 220);
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorear = "pink";
+while(l < lineas)
+{
+	yi=10*l;
+	xf=10*(l+1);
+	dibujarLinea("pink", 0, yi, xf, 300);	
+	console.log("Linea" +l);
+	l++;
+}
+for(var i=0; i<lineas; i++)
+{
+	dibujarLinea(colorear, 1, 1, 1, 299);
+    dibujarLinea(colorear, 1, 300,300, 299);
+}
 
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
@@ -14,4 +28,3 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 	lienzo.stroke();
 	lienzo.closePath();
 }
-
