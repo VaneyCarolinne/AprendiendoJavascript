c75ac310aec6f668f1f723038f2d622666233974
Vanessa Cruz <vaneycarolinne.vc@gmail.com>
2017-06-19 12:35:39 -0400

    Dibujando con las flechas del teclado

--- eventosteclas.js
+++ eventosteclas.js
@@ -10,22 +10,44 @@ var teclas = {
 console.log(teclas);
 
 document.addEventListener("keyup",dibujarTeclado);
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+dibujarLinea("pink", 100, 100, 200, 200, papel);
+var x=150;
+var y=150;
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.lineWidth = 4;
+	lienzo.moveTo(xinicial,yinicial);
+	lienzo.lineTo(xfinal,yfinal);
+	lienzo.stroke();
+	lienzo.closePath();
+}
+
 function dibujarTeclado(evento)
 {
+	var colorear = "pink";
+	var movimiento = 10;
 	if(evento.keyCode==teclas.UP)
 	{
-		console.log("Vamos pa arriba");
+		dibujarLinea(colorear, x, y, x, y - movimiento, papel);
+		y= y - movimiento;
 	}
 	if(evento.keyCode==teclas.DOWN)
 	{
-		console.log("Vamos pa abajo");
+		dibujarLinea(colorear, x, y, x, y + movimiento, papel);
+		y = y + movimiento;
 	}
 	if(evento.keyCode==teclas.LEFT)
 	{
-		console.log("Vamos pa la izquierda");
+		dibujarLinea(colorear, x, y, x - movimiento, y, papel);
+		x = x - movimiento;
 	}
 	if(evento.keyCode==teclas.RIGHT)
 	{
-		console.log("Vamos pa la derecha");
+		dibujarLinea(colorear, x, y, x + movimiento, y, papel);
+		x= x + movimiento;
 	}
 }
